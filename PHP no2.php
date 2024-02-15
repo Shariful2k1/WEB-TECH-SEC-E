816343276d43767a69c11a1de33ccd6d55fada5d
Shariful2k1 <158989798+Shariful2k1@users.noreply.github.com>
2024-02-15 14:00:06 +0600

    Add files via upload

---
+++ PHP no2.php
@@ -0,0 +1,7 @@
+<?php 
+
+$price = 14;  
+$vat = 0.15;  
+echo "Vat over the price is = " . ($price * $vat) . "<br />"; 
+
+?>
---
+++ PHP no3.php
@@ -0,0 +1,10 @@
+<?php
+$number = 33;
+
+if ($number % 2 == 0) {
+    echo "The number is even number.\n";
+} else {
+    echo "The number is odd number.\n";
+}
+    
+?>
