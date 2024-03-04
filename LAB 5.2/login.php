e4a39cef0cb4a0d64ff6353a648d5e4773505eec
Shariful2k1 <158989798+Shariful2k1@users.noreply.github.com>
2024-03-04 13:58:07 +0600

    Add files via upload

---
+++ LAB 5.2/login.php
@@ -0,0 +1,45 @@
+<html>
+    <head>
+        <title>Login</title>
+    </head>
+    <body>
+        <table border="1" align="center" width="750px">
+            <tr>
+                <td>
+                <form method="" action="" enctype="">
+                   XCompany &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                   <a href= "publichome.php">  Home </a> | <a href= "login.php"> Login </a> | <a href= "registration.php"> Registration </a>
+                </td>
+                <tr>
+                    <td>
+                    <center>
+                       <fieldset>
+                        <legend>LOGIN</legend>
+                        UserName : <input type="text" name="username" value="">
+                        <p>
+                        Password : <input type="password" name="password"
+                        value="">
+                        </p>
+                        <hr>
+                        <input type="checkbox" name="" value="">
+                        Remember Me
+                        <p>
+                        <a href="loggedin.php">
+                        <input type="button" name="submit" value="Submit"> </a> 
+                        <a href= "forgotpassword.php"> Forget Password? </a>
+                        </p>
+                        </fieldset>
+                    </center>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <center>
+                            Copyright &copy; 2017
+                        </center>
+                    </td>
+                </tr>
+            </tr>
+        </table>
+    </body>
+</html>
