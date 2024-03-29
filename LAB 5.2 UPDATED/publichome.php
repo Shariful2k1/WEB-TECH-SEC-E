9d570a6375f25781507f5e9efbd0c42ef93bbe95
Shariful2k1 <158989798+Shariful2k1@users.noreply.github.com>
2024-03-30 05:18:47 +0600

    Add files via upload

---
+++ LAB 5.2 UPDATED/publichome.php
@@ -0,0 +1,37 @@
+<html>
+    <head>
+        <title>Public Home</title>
+    </head>
+    <body>
+        <table border="1" align="center" width="650px">
+            <tr>
+                <td>
+                   <strong>XCompany</strong> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                   <a href= "#">  Home </a> | <a href= "login.php"> Login </a> | <a href= "registration.php"> Registration </a>
+                </td>
+                <tr>
+                    <td>
+                        <p>
+                            <h5>
+                                <br>
+                                <br>
+                                &nbsp;&nbsp;Welcome to xCompany
+                                <br>
+                                <br>
+                                <br>
+                                <br>
+                            </h5>
+                        </p>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <center>
+                            Copyright &copy; 2017
+                        </center>
+                    </td>
+                </tr>
+            </tr>
+        </table>
+    </body>
+</html>
---
+++ LAB 5.2 UPDATED/viewprofile.php
@@ -0,0 +1,54 @@
+<html>
+    <head>
+        <title>View Profile</title>
+    </head>
+    <body>
+        <table border="1" align="center" width="650px">
+            <tbody>
+            <tr>
+                <td colspan="2">
+                   <strong>XCompany</strong> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                   Logged in as Bob | <a href="logout.php"> Logout </a>
+                
+                <tr>
+                    <td>
+                         <strong>Account</strong>
+                         <hr>
+                         <ul>
+                            <li><a href="loggedin.php"> Dashboard </a></li>
+                            <li><a href="#"> View Profile </a></li>
+                            <li><a href="editprofile.php"> Edit Profile </a></li>
+                            <li><a href="changeprofilepicture.php"> Change Profile Picture</a></li>
+                            <li><a href="changepassword.php"> Change Password </a></li>
+                            <li><a href="publichome.php"> Logout </a></li>
+                         </ul>
+                    </td>
+                    <td>
+                        <fieldset>
+                            <legend>PROFILE</legend>
+                            Name: Bob
+                            <hr>
+                            Email: bob@aiub.edu
+                            <hr>
+                            Gender: Male
+                            <hr>
+                            Date of birth: 19/09/1998
+                            <hr>
+                            <a href="editprofile.php">Edit Profile</a>
+                            <td rowspan="3"><img src="ppimg.png" alt="profile picture" width="120px"><a href="changepp.php"><br>Change</a>
+                        </fieldset>
+                    </td>
+                </tr>
+                </td>
+                <tr>
+                    <td colspan="2">
+                        <center>
+                            Copyright &copy; 2017
+                        </center>
+                    </td>
+                </tr>
+            </tr>
+            </tbody>
+        </table>
+    </body>
+</html>
